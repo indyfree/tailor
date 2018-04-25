a73860a445f74dc5a3f3bae23dbff5b8ac932f21
Tobias Richter <tr4711@gmail.com>
2018-04-25 15:18:22 +0200

    Add initial project structure (#7)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='dsip1',
+    author='niki, chrissy, tobi',
+)
