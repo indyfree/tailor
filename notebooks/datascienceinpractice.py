045ee8171f71e4432db051e0c2a5f13d6f4f7185
Tobias Richter <tr4711@gmail.com>
2018-04-26 15:24:07 +0200

    Test diff github

---
+++ notebooks/datascienceinpractice.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# # Hauptseminar Advanced Data Science in Practice
+
+# ## Gruppe: 1
+
+# ### Erster
+
+# In[4]:
+
+
+Stud1 = 'Reinhold, Niklas, 5377277, nreinhol' 
+Stud2 = 'Richter, Tobias, 5583705, indyfree' 
+Stud3 = 'Bullmann, Christoph Alexander, 5585627, bullmanc' 
+
+
+# ## Import
+
+# In[13]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import numpy
+import pandas as pa
+import matplotlib.pyplot as plt
+
+
+# In[14]:
+
+
+plt.plot([1,2,3,3])
+plt.ylabel('some numbers')
+plt.show()
+
