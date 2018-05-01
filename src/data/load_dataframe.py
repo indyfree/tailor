a6663de5bc1bb9ce9dfb924e394019ab07cb4ff7
nreinhol <nreinhold24@gmail.com>
2018-05-01 17:36:49 +0200

    Add load dataframe
    * add pandas to requirements
    
    * add method to create dataframe
    
    * fix method to create dataframe
    
    * fix 2 method to create dataframe
    
    * fix3 method to create dataframe

---
+++ src/data/load_dataframe.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import pandas as pd
+
+
+def load_raw_dataframe():
+    PROJECT_DIR = str(Path(__file__).resolve().parents[2])
+    RAW_DATA_DIR = '/data/raw/'
+    RAW_DATA_FILE = 'data.csv'
+    raw_dataframe = load_dataframe(PROJECT_DIR + RAW_DATA_DIR + RAW_DATA_FILE)
+
+    return raw_dataframe
+
+
+def load_dataframe(FILE_PATH):
+    dataframe = pd.read_csv(FILE_PATH, encoding='iso-8859-1')
+
+    return dataframe
+
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
