4d817118451a694735c0a40b8251ab4b5d035811
Tobias Richter <tr4711@gmail.com>
2018-05-06 20:52:36 +0200

    Specify package directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tailor',
-    packages=find_packages(),
+    packages=find_packages('src'),
     version='0.0.1',
     description='dsip1',
     author='Niklas R., Christoph B., Tobias R.',
