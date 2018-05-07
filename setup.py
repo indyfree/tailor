85def3e1ce12c1e4518696239764548a93112c55
Tobias Richter <tr4711@gmail.com>
2018-05-07 22:17:28 +0200

    Rename project to tailor (#35)

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='tailor',
+    packages=find_packages('src'),
     version='0.0.1',
     description='dsip1',
-    author='niki, chrissy, tobi',
+    author='Niklas R., Christoph B., Tobias R.',
+    package_dir={"": "src"},
 )
