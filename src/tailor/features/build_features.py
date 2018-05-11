062b782352d170e58a3a48abb8f345aed9cdfda8
Niklas Reinhold <nreinhold24@gmail.com>
2018-05-11 17:56:59 +0200

    add feature to summarize performance measures

--- src/tailor/features/build_features.py
+++ src/tailor/features/build_features.py
@@ -3,3 +3,15 @@ def group_dataframe_by_attribute(dataframe, attribute, mean=False):
         return dataframe.groupby(attribute, as_index=False).mean()
     else:
         return dataframe.groupby(attribute, as_index=False).sum()
+
+
+'''
+Summarize sells, revenue and avq for every time_on_sale value for any feature characteristic
+'''
+
+
+def summarize_performance_measures_for_feature_characteristic(dataframe, column_name_of_feature, feature_characteristic):
+    dataframe = dataframe[dataframe[column_name_of_feature] == feature_characteristic]
+    grouped_dataframe = group_dataframe_by_attribute(dataframe, 'time_on_sale')
+
+    return grouped_dataframe[['time_on_sale', 'revenue', 'avq', 'article_count']]
