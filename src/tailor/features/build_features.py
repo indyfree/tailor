3bdd3eb17cc5b1796bb87e9de4fd0e1567b0f3c0
Niklas Reinhold <nreinhold24@gmail.com>
2018-05-14 16:14:43 +0200

    rename function and variables

--- src/tailor/features/build_features.py
+++ src/tailor/features/build_features.py
@@ -5,13 +5,10 @@ def group(dataframe, attribute, mean=False):
         return dataframe.groupby(attribute, as_index=False).sum()
 
 
-'''
-Summarize sells, revenue and avq for every time_on_sale value for any feature characteristic
-'''
+def get_performance_measures(dataframe, column, characteristic):
+    '''Summarize sells, revenue and avq for every time_on_sale value for any feature characteristic'''
 
-
-def summarize_performance_measures_for_feature_characteristic(dataframe, column_name_of_feature, feature_characteristic):
-    dataframe = dataframe[dataframe[column_name_of_feature] == feature_characteristic]
-    grouped_dataframe = group_dataframe_by_attribute(dataframe, 'time_on_sale')
+    filtered_dataframe = dataframe[dataframe[column] == characteristic]
+    grouped_dataframe = group(filtered_dataframe, 'time_on_sale')
 
     return grouped_dataframe[['time_on_sale', 'revenue', 'avq', 'article_count']]
