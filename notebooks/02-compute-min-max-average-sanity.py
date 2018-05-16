5c9952b89d7bccc51123e48b0ed181ece418d1cf
Christoph Alexander Bullmann <bullmanc@smail.uni-koeln.de>
2018-05-16 17:05:14 +0200

    Adjust sanity notebook with comments and nicer structure

--- notebooks/02-compute-min-max-average-sanity.py
+++ notebooks/02-compute-min-max-average-sanity.py
@@ -4,28 +4,30 @@
 # In[1]:
 
 
-import pandas as pd
-import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tailor.data.load_dataframe as ld
 
 
 # In[2]:
 
 
-import tailor.data.load_dataframe as ld
+data_raw = ld.load_raw_dataframe()
 
 
 # In[3]:
 
 
-data_raw = ld.load_raw_dataframe()
+data_raw.head()
 
 
 # In[4]:
 
 
-data_raw
+# checking whether there are any NaN cells
+data_raw.isnull().sum()
 
 
 # In[5]:
@@ -43,107 +45,137 @@ data_raw.max()
 # In[7]:
 
 
-data_raw.season.unique()
+# checking whether article_id unique count matches with min max values
+np.count_nonzero(data_raw.article_id.unique())
 
 
 # In[8]:
 
 
-data_raw.brand.unique()
+# describe() is pretty useless for transaction_date, markdown_start_date, markdown_end_date
 
 
 # In[9]:
 
 
-data_raw.original_price.mean()
+# change float format for better readability, without it describe() shows x.xxxxxxxxxe+xx
+pd.options.display.float_format = "{:.2f}".format
+# the 50% of the describe output is the median
+data_raw.original_price.describe()
 
 
 # In[10]:
 
 
-data_raw.sells_price.mean()
+data_raw.sells_price.describe()
 
 
 # In[11]:
 
 
-data_raw.discount.mean()
+data_raw.discount.describe()
 
 
 # In[12]:
 
 
-data_raw.discount.median()
+data_raw.markdown.describe()
 
 
 # In[13]:
 
 
-data_raw.markdown.mean()
+data_raw.article_count.describe()
 
 
 # In[14]:
 
 
-data_raw.markdown.median()
+data_raw.revenue.describe()
 
 
 # In[15]:
 
 
-data_raw.article_count.mean()
+# there are some really ridiculous outliers here
+data_raw.stock_total.describe()
 
 
 # In[16]:
 
 
-data_raw.article_count.median()
+# max value of this is above 100%, probably an accumulated rounding error, but still there
+data_raw.avq.describe()
 
 
 # In[17]:
 
 
-data_raw.color.unique()
+data_raw.time_on_sale.describe()
 
 
 # In[18]:
 
 
-data_raw.color.value_counts()
+data_raw.season.unique()
 
 
 # In[19]:
 
 
-data_raw.brand.value_counts()
+data_raw.season.value_counts()
 
 
 # In[20]:
 
 
-data_raw.season.value_counts()
+data_raw.brand.unique()
+
+
+# In[21]:
+
+
+data_raw.brand.value_counts()
+
+
+# In[22]:
+
+
+data_raw.color.unique()
 
 
 # In[23]:
 
 
-np.count_nonzero(data_raw.article_id.unique())
+data_raw.color.value_counts()
 
 
 # In[24]:
 
 
-np.count_nonzero(data_raw.article_id)
+sorted(data_raw.Abteilung.unique())
 
 
 # In[25]:
 
 
-1131329-608555
+sorted(data_raw.WHG.unique())
 
 
 # In[26]:
 
 
-data_raw.isnull().sum()
+data_raw.WHG.value_counts()
+
+
+# In[27]:
+
+
+sorted(data_raw.WUG.unique())
+
+
+# In[28]:
+
+
+data_raw.WUG.value_counts()
 
