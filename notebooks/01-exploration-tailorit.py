4d53154af501f85c2b5cc1533c048dea17211c5f
Tobias Richter <tr4711@gmail.com>
2018-05-17 23:59:07 +0200

    Adjust notebook with more explaination

--- notebooks/01-exploration-tailorit.py
+++ notebooks/01-exploration-tailorit.py
@@ -24,6 +24,7 @@ import numpy as np
 import pandas as pd
 
 import tailor
+from tailor.data import group_by
 
 
 # ### Load data
@@ -32,6 +33,7 @@ import tailor
 
 
 df = tailor.load_data()
+df = group_by.weeks_on_sale(df)
 
 
 # ### Get an overview over the dataset
@@ -65,10 +67,13 @@ df.isna().values.any()
 
 
 article = df.loc[df.article_id == 900003, [
-    "time_on_sale", "article_count", "avq", "revenue"]]
-plt.plot(article.time_on_sale, article.article_count, 'blue')
-plt.plot(article.time_on_sale, article.revenue/40, 'orange')
-plt.plot(article.time_on_sale, article.avq, 'green')
-plt.xlabel('time on sale')
+    "weeks_on_sale", "article_count", "avq", "revenue", "sells_price", "original_price"]]
+plt.plot(article.weeks_on_sale, article.article_count, 'blue')
+plt.plot(article.weeks_on_sale, article.revenue/article.original_price, 'orange', label='revenue')
+plt.plot(article.weeks_on_sale, article.avq, 'green')
+plt.plot(article.weeks_on_sale, article.sells_price, 'red')
+plt.xlabel('weeks on sale')
 plt.legend();
 
+
+# We can see the clear relationship between the amount of articles sold, it's sells price (including markdown and discounts) and the generated revenue and here.
