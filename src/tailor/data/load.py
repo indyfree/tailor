02cf54a5f6df8a3dc1e69bdd081b08b1bdadeae7
Tobias Richter <tr4711@gmail.com>
2018-05-18 19:31:53 +0200

    Move transformation logic into proces.py

--- src/tailor/data/load.py
+++ src/tailor/data/load.py
@@ -1,31 +1,14 @@
 import pandas as pd
 
 import tailor
-from tailor import features
 
 
+# PROCESSED_DATA_FILE = tailor.PROJECT_DIR + '/data/processed/data.pkl'
 def load_data():
-    '''Loads the raw data and returns a dataframe with formatted columns'''
+    return null
 
-    df = load_csv()
-
-    # Set article_id as category despite it's beeing a number
-    df["article_id"] = df["article_id"].astype('category')
-
-    # Set dates as type datetime
-    df["transaction_date"] = pd.to_datetime(df["transaction_date"])
-    df["markdown_start_date"] = pd.to_datetime(df["markdown_start_date"])
-    # Replace 'infity' date with computable date in the future
-    df.loc[df.markdown_end_date == "9999-12-31", "markdown_end_date"] = "2018-12-31"
-    df["markdown_end_date"] = pd.to_datetime(df["markdown_end_date"])
-
-    # Set all other types as categories
-    df[df.select_dtypes(['object']).columns] = df.select_dtypes(['object']).apply(lambda x: x.astype('category'))
-
-    # Build new features
-    df = features.build(df)
-
-    return df
+    
+#     # if os.path.isfile(PROCESSED_DATA_FILE) is False:
 
 
 def load_csv():
---
+++ src/tailor/data/process.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def transform_datatypes(df):
+    '''Transform the raw data and returns a dataframe with correct dataypes'''
+
+    # Set article_id as category despite it's beeing a number
+    df["article_id"] = df["article_id"].astype('category')
+
+    # Set dates as type datetime
+    df["transaction_date"] = pd.to_datetime(df["transaction_date"])
+    df["markdown_start_date"] = pd.to_datetime(df["markdown_start_date"])
+    # Replace 'infity' date with computable date in the future
+    df.loc[df.markdown_end_date == "9999-12-31", "markdown_end_date"] = "2018-12-31"
+    df["markdown_end_date"] = pd.to_datetime(df["markdown_end_date"])
+
+    # Set all other types as categories
+    df[df.select_dtypes(['object']).columns] = df.select_dtypes(['object']).apply(lambda x: x.astype('category'))
+
+    return df
