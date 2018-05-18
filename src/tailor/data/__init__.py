f16a4481cde43bc7965f46bb05b3015006450914
Tobias Richter <tr4711@gmail.com>
2018-05-18 19:33:15 +0200

    Fix imports

--- src/tailor/data/__init__.py
+++ src/tailor/data/__init__.py
@@ -1,3 +1,8 @@
 from . import group_by # noqa
 from .load import load_data # noqa
-from .load import PROJECT_DIR # noqa
+from .load import load_csv # noqa
+from .process import transform_datatypes # noqa
+
+from pathlib import Path
+
+PROJECT_DIR = str(Path(__file__).resolve().parents[3])
