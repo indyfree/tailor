983c2a437c70d0e45582da118e3ffebf38646911
Tobias Richter <tr4711@gmail.com>
2018-05-19 23:01:14 +0200

    Fix imports and add constants to package

--- src/tailor/data/__init__.py
+++ src/tailor/data/__init__.py
@@ -1,8 +1,15 @@
 from . import group_by # noqa
 from .load import load_data # noqa
 from .load import load_csv # noqa
+from .make_dataset import process_data # noqa
+from .make_dataset import download_data # noqa
 from .process import transform_datatypes # noqa
 
 from pathlib import Path
 
+# Constants
 PROJECT_DIR = str(Path(__file__).resolve().parents[3])
+RAW_DATA_PATH = PROJECT_DIR + '/data/raw'
+RAW_DATA_FILE = RAW_DATA_PATH + '/data.csv'
+PROCESSED_DATA_PATH = PROJECT_DIR + '/data/processed'
+PROCESSED_DATA_FILE = PROCESSED_DATA_PATH + '/data.pkl'
