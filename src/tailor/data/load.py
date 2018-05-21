869496052273933fd4b12e801da0a94fc1e57d65
Tobias Richter <tr4711@gmail.com>
2018-05-21 14:16:35 +0200

    Save and load processed data from file (#54)

--- src/tailor/data/load.py
+++ src/tailor/data/load.py
@@ -1,34 +1,21 @@
+import os
 import pandas as pd
 
-import tailor
-from tailor import features
+from tailor import data
 
 
-def load_data():
-    '''Loads the raw data and returns a dataframe with formatted columns'''
+def load_data(rebuild=False):
+    '''Loads processed data into a dataframe, process again if needed'''
+    if rebuild is True:
+        data.process_data()
+    elif os.path.isfile(data.PROCESSED_DATA_FILE) is False:
+        data.process_data()
 
-    df = load_csv()
-
-    # Set article_id as category despite it's beeing a number
-    df["article_id"] = df["article_id"].astype('category')
-
-    # Set dates as type datetime
-    df["transaction_date"] = pd.to_datetime(df["transaction_date"])
-    df["markdown_start_date"] = pd.to_datetime(df["markdown_start_date"])
-    # Replace 'infity' date with computable date in the future
-    df.loc[df.markdown_end_date == "9999-12-31", "markdown_end_date"] = "2018-12-31"
-    df["markdown_end_date"] = pd.to_datetime(df["markdown_end_date"])
-
-    # Set all other types as categories
-    df[df.select_dtypes(['object']).columns] = df.select_dtypes(['object']).apply(lambda x: x.astype('category'))
-
-    # Build new features
-    df = features.build(df)
-
-    return df
+    return pd.read_pickle(data.PROCESSED_DATA_FILE)
 
 
 def load_csv():
-    RAW_DATA_FILE = tailor.PROJECT_DIR + '/data/raw/data.csv'
+    if os.path.isfile(data.RAW_DATA_FILE) is False:
+        data.download_data()
 
-    return pd.read_csv(RAW_DATA_FILE, encoding='iso-8859-1')
+    return pd.read_csv(data.RAW_DATA_FILE, encoding='iso-8859-1')
