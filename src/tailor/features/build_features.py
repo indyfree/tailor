8d9d0414b72e0305c4b296a6b1d986dc65198bfc
Christoph Alexander Bullmann <bullmanc@smail.uni-koeln.de>
2018-05-22 15:15:28 +0200

    add build function and call

--- src/tailor/features/build_features.py
+++ src/tailor/features/build_features.py
@@ -5,6 +5,7 @@ def build(df):
     '''Build new features'''
 
     df = weeks_on_sale(df)
+    df = expand_date_info(df)
     return df
 
 
@@ -26,3 +27,35 @@ def weeks_on_sale(df):
 
 def days_to_week(days):
     return days // 7
+
+
+def meteor_season(month)
+    '''return meteorological season'''
+
+    if 2 < month < 6:
+        return 'spring'
+    elif 5 < month < 9:
+        return 'summer'
+    elif 8 < month < 12:
+        return 'fall'
+    else:
+        return 'winter'
+
+
+def expand_date_info(df)
+    '''Calculate the weekday, month and actual season'''
+
+    season = list()
+    weekday = list()
+    months = list()
+
+    # three different functions therefore faster than three separate .apply()
+    for i in df.transaction_date:
+        month = i.month
+        season.append(meteor_season(month))
+        months.append(month)
+        weekday.append(i.weekday())
+
+    df['season_buy'] = pd.Series(season, index=df.index)
+    df['month'] = pd.Series(months, index=df.index)
+    df['weekday'] = pd.Series(weekday, index=df.index)
