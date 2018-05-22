9f2403d3e3bc1359e2b1cba2b2759388d0b029a3
Christoph Alexander Bullmann <bullmanc@smail.uni-koeln.de>
2018-05-22 16:57:15 +0200

    rerun notebook and add markdown

--- notebooks/03-feature-generation.py
+++ notebooks/03-feature-generation.py
@@ -1,34 +1,155 @@
 
 # coding: utf-8
 
+# # Feature Generation and Timing
+
+# ## Initialization
+
 # In[1]:
 
 
 import numpy as np
 import pandas as pd
-import tailor
+from tailor import data
 
 
 # In[2]:
 
 
-df = tailor.load_data()
+df = data.load_csv()
 
 
-# In[ ]:
+# In[3]:
+
+
+# because we need to have datetime category for transaction_date
+df = data.transform_datatypes(df)
+
+
+# In[4]:
 
 
 df.head()
 
 
-# In[ ]:
+# ## Comparison
+
+# ### timing on big scale
+
+# In[5]:
 
 
-get_ipython().run_cell_magic('time', '', '\n# expand transaction_date information\nseason = list()\nweekday = list()\nmonths = list()\n\nfor i in df.transaction_date:\n    month = i.month\n    # meteorological seasons\n    if 2 < month < 6:\n        season.append(\'spring\')\n    elif 5 < month < 9:\n        season.append("summer")\n    elif 8 < month < 12:\n        season.append("fall")\n    else:\n        season.append("winter")\n\n    months.append(month)\n    day = i.weekday()\n    weekday.append(day)\n\ndf[\'season_buy\'] = pd.Series(season, index=df.index)\ndf[\'month\'] = pd.Series(months, index=df.index)\ndf[\'weekday\'] = pd.Series(weekday, index=df.index)')
+get_ipython().run_line_magic('', 'time')
+
+# expand transaction_date information
+season = list()
+weekday = list()
+months = list()
+
+for i in df.transaction_date:
+    month = i.month
+    # meteorological seasons
+    if 2 < month < 6:
+        season.append('spring')
+    elif 5 < month < 9:
+        season.append("summer")
+    elif 8 < month < 12:
+        season.append("fall")
+    else:
+        season.append("winter")
+
+    months.append(month)
+    day = i.weekday()
+    weekday.append(day)
+
+df['season_buy'] = pd.Series(season, index=df.index)
+df['month'] = pd.Series(months, index=df.index)
+df['weekday'] = pd.Series(weekday, index=df.index)
 
 
-# In[ ]:
+# In[6]:
 
 
-get_ipython().run_cell_magic('time', '', '\n# expand transaction_date information\n\ndef get_season(month):\n    if 2 < month < 6:\n        return \'spring\'\n    elif 5 < month < 9:\n        return "summer"\n    elif 8 < month < 12:\n        return "fall"\n    else:\n        return "winter"\n\ndf[\'month\'] = df[\'transaction_date\'].apply(lambda x : x.month)\ndf[\'season_buy\'] = df[\'month\'].apply(lambda x : get_season(x))\ndf[\'weekday\'] = df[\'transaction_date\'].apply(lambda x : x.weekday())')
+get_ipython().run_line_magic('', 'time')
+
+# expand transaction_date information
+
+
+def get_season(month):
+    if 2 < month < 6:
+        return 'spring'
+    elif 5 < month < 9:
+        return "summer"
+    elif 8 < month < 12:
+        return "fall"
+    else:
+        return "winter"
+
+
+df['month'] = df['transaction_date'].apply(lambda x: x.month)
+df['season_buy'] = df['month'].apply(lambda x: get_season(x))
+df['weekday'] = df['transaction_date'].apply(lambda x: x.weekday())
+
+
+# ### timing on small scale
+
+# In[7]:
+
+
+df = df.head()
+
+
+# In[8]:
+
+
+get_ipython().run_line_magic('', 'time')
+
+# expand transaction_date information
+season = list()
+weekday = list()
+months = list()
+
+for i in df.transaction_date:
+    month = i.month
+    # meteorological seasons
+    if 2 < month < 6:
+        season.append('spring')
+    elif 5 < month < 9:
+        season.append("summer")
+    elif 8 < month < 12:
+        season.append("fall")
+    else:
+        season.append("winter")
+
+    months.append(month)
+    day = i.weekday()
+    weekday.append(day)
+
+df['season_buy'] = pd.Series(season, index=df.index)
+df['month'] = pd.Series(months, index=df.index)
+df['weekday'] = pd.Series(weekday, index=df.index)
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('', 'time')
+
+# expand transaction_date information
+
+
+def get_season(month):
+    if 2 < month < 6:
+        return 'spring'
+    elif 5 < month < 9:
+        return "summer"
+    elif 8 < month < 12:
+        return "fall"
+    else:
+        return "winter"
+
+
+df['month'] = df['transaction_date'].apply(lambda x: x.month)
+df['season_buy'] = df['month'].apply(lambda x: get_season(x))
+df['weekday'] = df['transaction_date'].apply(lambda x: x.weekday())
 
