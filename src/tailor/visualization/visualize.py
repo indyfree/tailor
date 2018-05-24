63e917b16ad123ad56eb01f8415e807dab3f65cc
Tobias Richter <tr4711@gmail.com>
2018-05-24 16:44:09 +0200

    Plot article history for any number of articles

--- src/tailor/visualization/visualize.py
+++ src/tailor/visualization/visualize.py
@@ -3,45 +3,25 @@ import matplotlib.pyplot as plt
 import tailor
 from tailor.data import group_by
 
-OUTPUT_DIR = tailor.PROJECT_DIR + '/reports/figures/'
 
-
-def plot_line_chart(x_axis, y_axis, filename, x_axis_label='', y_axis_label=''):
+def plot_article_history(df, articles, measure):
     plt.figure()
     ax = plt.axes()
-    ax.set_ylabel(y_axis_label, fontsize=12)
-    ax.set_xlabel(x_axis_label, fontsize=12)
-    plt.plot(x_axis, y_axis)
-    plt.savefig(OUTPUT_DIR + filename)
+    ax.set_ylabel(measure, fontsize=12)
+    ax.set_xlabel('weeks on sale', fontsize=12)
 
+    for a in articles:
+        x = df.loc[df.article_id == a, 'weeks_on_sale']
+        y = df.loc[df.article_id == a, measure]
+        plt.plot(x, y, label=str(a))
 
-def plot_bar_chart(x_axis, y_axis, filename, x_axis_label='', y_axis_label=''):
-    plt.figure()
-    ax = plt.axes()
-    ax.set_ylabel(y_axis_label, fontsize=12)
-    ax.set_xlabel(x_axis_label, fontsize=12)
-    plt.bar(x_axis, y_axis)
-    plt.savefig(OUTPUT_DIR + filename)
-
-
-def plot_scatter_plot(x_axis, y_axis, filename, x_axis_label='', y_axis_label=''):
-    plt.figure()
-    ax = plt.axes()
-    ax.set_ylabel(y_axis_label, fontsize=12)
-    ax.set_xlabel(x_axis_label, fontsize=12)
-    plt.scatter(x_axis, y_axis, alpha=0.3, cmap='viridis', s=1.5)
-    plt.savefig(OUTPUT_DIR + filename)
+    plt.legend()
 
 
 def main():
-    raw_dataframe = tailor.load_data()
-    grouped_dataframe = group_by.attribute(raw_dataframe, 'article_id', mean=True)
-
-    plot_bar_chart(grouped_dataframe['article_id'], grouped_dataframe['revenue'], 'barchart_mean_revenue.png')
-
-    plot_bar_chart(grouped_dataframe['article_id'], grouped_dataframe['avq'], 'barchart_mean_avq.png')
-
-    plot_bar_chart(grouped_dataframe['article_id'], grouped_dataframe['article_count'], 'barchart_mean_article_count.png')
+    df = tailor.load_data()
+    df = group_by.weeks_on_sale(df)
+    plot_article_history(df, [900001, 900002], 'revenue')
 
 
 if __name__ == '__main__':
