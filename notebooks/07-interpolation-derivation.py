b67d4b3403ed2e555fb0270300135e0d1569d38c
Christoph Alexander Bullmann <bullmanc@smail.uni-koeln.de>
2018-06-08 18:14:25 +0200

    improve notebook

--- notebooks/07-interpolation-derivation.py
+++ notebooks/07-interpolation-derivation.py
@@ -80,8 +80,6 @@ pdf, = plt.plot(xf, ydf, label='derivation')
 plt.legend(handles=[pod, pif, pdf])
 
 
-# ## Distance Calculation Timing
-
 # In[9]:
 
 
@@ -94,14 +92,35 @@ get_ipython().run_cell_magic('time', '', 'cl.euclidean(series_a,series_b)')
 get_ipython().run_cell_magic('time', '', 'cl.derivative_euclidean(series_a,series_b, degree=1, smooth=2)')
 
 
-# ## Curve Comparison
+# ## Distance and curve comparison
 
 # In[11]:
 
 
-# generate the functions
-f1 = cl.interpolate_function(series_a, smooth=2, degree=1)
-f2 = cl.interpolate_function(series_b, smooth=2, degree=1)
+print("Base: ")
+print("Euclidean distance: " + np.str(cl.euclidean(series_a, series_b)))
+print(" ")
+print("Interpolation and derivation: ")
+print("Degree 1 distance: " + np.str(cl.derivative_euclidean(series_a,series_b, degree=1, smooth=2)))
+print("Degree 2 distance: " + np.str(cl.derivative_euclidean(series_a,series_b, degree=2, smooth=2)))
+print("Degree 3 distance: " + np.str(cl.derivative_euclidean(series_a,series_b, degree=3, smooth=2)))
+print("Degree 4 distance: " + np.str(cl.derivative_euclidean(series_a,series_b, degree=4, smooth=2)))
+print("Degree 5 distance: " + np.str(cl.derivative_euclidean(series_a,series_b, degree=5, smooth=2)))
+
+
+# The derivate of the interpolation with degree=1 gives the best results and is better than the base distance measurement. Interpolation with higher degrees results in distorted curves and performs worse than the base measure. In the following subsections each degree can be viewed in detail.
+
+# ### Degree 1
+
+# In[12]:
+
+
+get_ipython().run_cell_magic('time', '', '\n# generate the functions\nf1 = cl.interpolate_function(series_a, degree=1, smooth=2)\nf2 = cl.interpolate_function(series_b, degree=1, smooth=2)')
+
+
+# In[13]:
+
+
 # generate fine resolution x axis
 xf = np.linspace(min(series_a.index.min(), series_b.index.min()), max(series_a.index.max(), series_b.index.max()), 1000)
 # generate fine resolution y values
@@ -119,7 +138,7 @@ pod2, = plt.plot(series_b.index, series_b.values, '.-', label='original data b')
 plt.legend(handles=[pod1, pod2])
 
 
-# In[12]:
+# In[14]:
 
 
 # plot interpolated function
@@ -128,7 +147,7 @@ pif2, = plt.plot(xf, yf2, label='interpolation b')
 plt.legend(handles=[pif1, pif2])
 
 
-# In[13]:
+# In[15]:
 
 
 # plot derivative function
@@ -137,4 +156,186 @@ pdf2, = plt.plot(xf, ydf2, label='derivation b')
 plt.legend(handles=[pdf1, pdf2])
 
 
-# Seeing 
+# ### Degree 2 
+
+# In[16]:
+
+
+get_ipython().run_cell_magic('time', '', '\n# generate the functions\nf1 = cl.interpolate_function(series_a, degree=2, smooth=2)\nf2 = cl.interpolate_function(series_b, degree=2, smooth=2)')
+
+
+# In[17]:
+
+
+# generate fine resolution x axis
+xf = np.linspace(min(series_a.index.min(), series_b.index.min()), max(series_a.index.max(), series_b.index.max()), 1000)
+# generate fine resolution y values
+yf1 = f1(xf)
+yf2 = f2(xf)
+# generate derivative function
+df1 = f1.derivative()
+df2 = f2.derivative()
+# generate fine resolution derivative y values
+ydf1 = df1(xf)
+ydf2 = df2(xf)
+# plot original data-points
+pod1, = plt.plot(series_a.index, series_a.values, '.-', label='original data a')
+pod2, = plt.plot(series_b.index, series_b.values, '.-', label='original data b')
+plt.legend(handles=[pod1, pod2])
+
+
+# In[18]:
+
+
+# plot interpolated function
+pif1, = plt.plot(xf, yf1, label='interpolation a')
+pif2, = plt.plot(xf, yf2, label='interpolation b')
+plt.legend(handles=[pif1, pif2])
+
+
+# In[19]:
+
+
+# plot derivative function
+pdf1, = plt.plot(xf, ydf1, label='derivation a')
+pdf2, = plt.plot(xf, ydf2, label='derivation b')
+plt.legend(handles=[pdf1, pdf2])
+
+
+# ### Degree 3
+
+# In[20]:
+
+
+get_ipython().run_cell_magic('time', '', '\n# generate the functions\nf1 = cl.interpolate_function(series_a, degree=3, smooth=2)\nf2 = cl.interpolate_function(series_b, degree=3, smooth=2)')
+
+
+# In[21]:
+
+
+# generate fine resolution x axis
+xf = np.linspace(min(series_a.index.min(), series_b.index.min()), max(series_a.index.max(), series_b.index.max()), 1000)
+# generate fine resolution y values
+yf1 = f1(xf)
+yf2 = f2(xf)
+# generate derivative function
+df1 = f1.derivative()
+df2 = f2.derivative()
+# generate fine resolution derivative y values
+ydf1 = df1(xf)
+ydf2 = df2(xf)
+# plot original data-points
+pod1, = plt.plot(series_a.index, series_a.values, '.-', label='original data a')
+pod2, = plt.plot(series_b.index, series_b.values, '.-', label='original data b')
+plt.legend(handles=[pod1, pod2])
+
+
+# In[22]:
+
+
+# plot interpolated function
+pif1, = plt.plot(xf, yf1, label='interpolation a')
+pif2, = plt.plot(xf, yf2, label='interpolation b')
+plt.legend(handles=[pif1, pif2])
+
+
+# In[23]:
+
+
+# plot derivative function
+pdf1, = plt.plot(xf, ydf1, label='derivation a')
+pdf2, = plt.plot(xf, ydf2, label='derivation b')
+plt.legend(handles=[pdf1, pdf2])
+
+
+# ### Degree 4
+
+# In[24]:
+
+
+get_ipython().run_cell_magic('time', '', '\n# generate the functions\nf1 = cl.interpolate_function(series_a, degree=4, smooth=2)\nf2 = cl.interpolate_function(series_b, degree=4, smooth=2)')
+
+
+# In[25]:
+
+
+# generate fine resolution x axis
+xf = np.linspace(min(series_a.index.min(), series_b.index.min()), max(series_a.index.max(), series_b.index.max()), 1000)
+# generate fine resolution y values
+yf1 = f1(xf)
+yf2 = f2(xf)
+# generate derivative function
+df1 = f1.derivative()
+df2 = f2.derivative()
+# generate fine resolution derivative y values
+ydf1 = df1(xf)
+ydf2 = df2(xf)
+# plot original data-points
+pod1, = plt.plot(series_a.index, series_a.values, '.-', label='original data a')
+pod2, = plt.plot(series_b.index, series_b.values, '.-', label='original data b')
+plt.legend(handles=[pod1, pod2])
+
+
+# In[26]:
+
+
+# plot interpolated function
+pif1, = plt.plot(xf, yf1, label='interpolation a')
+pif2, = plt.plot(xf, yf2, label='interpolation b')
+plt.legend(handles=[pif1, pif2])
+
+
+# In[27]:
+
+
+# plot derivative function
+pdf1, = plt.plot(xf, ydf1, label='derivation a')
+pdf2, = plt.plot(xf, ydf2, label='derivation b')
+plt.legend(handles=[pdf1, pdf2])
+
+
+# ### Degree 5
+
+# In[28]:
+
+
+get_ipython().run_cell_magic('time', '', '\n# generate the functions\nf1 = cl.interpolate_function(series_a, degree=5, smooth=2)\nf2 = cl.interpolate_function(series_b, degree=5, smooth=2)')
+
+
+# In[29]:
+
+
+# generate fine resolution x axis
+xf = np.linspace(min(series_a.index.min(), series_b.index.min()), max(series_a.index.max(), series_b.index.max()), 1000)
+# generate fine resolution y values
+yf1 = f1(xf)
+yf2 = f2(xf)
+# generate derivative function
+df1 = f1.derivative()
+df2 = f2.derivative()
+# generate fine resolution derivative y values
+ydf1 = df1(xf)
+ydf2 = df2(xf)
+# plot original data-points
+pod1, = plt.plot(series_a.index, series_a.values, '.-', label='original data a')
+pod2, = plt.plot(series_b.index, series_b.values, '.-', label='original data b')
+plt.legend(handles=[pod1, pod2])
+
+
+# In[30]:
+
+
+# plot interpolated function
+pif1, = plt.plot(xf, yf1, label='interpolation a')
+pif2, = plt.plot(xf, yf2, label='interpolation b')
+plt.legend(handles=[pif1, pif2])
+
+
+# In[31]:
+
+
+# plot derivative function
+pdf1, = plt.plot(xf, ydf1, label='derivation a')
+pdf2, = plt.plot(xf, ydf2, label='derivation b')
+plt.legend(handles=[pdf1, pdf2])
+
