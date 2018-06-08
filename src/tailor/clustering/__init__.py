db87a58c434ab401f93fa92b5c75d6c5c0dc5593
Christoph Bullmann <38309444+bullmanc@users.noreply.github.com>
2018-06-08 18:21:47 +0200

    Add Distance measures (#64)
    * add rebuild season feature
    
    * fix lint
    
    * fix lint
    
    * add functions and testing
    
    * reset to master
    
    * fix lint
    
    * fix typo
    
    * fix lint
    
    * improve code and notebook
    
    * improve notebook
    
    * fix lint

--- src/tailor/clustering/__init__.py
+++ src/tailor/clustering/__init__.py
@@ -1,3 +1,5 @@
 from .cluster import cluster # noqa
 from .ranking import rank_features # noqa
 from .distance import euclidean # noqa
+from .distance import interpolate_function # noqa
+from .distance import derivative_euclidean # noqa
