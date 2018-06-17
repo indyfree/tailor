40a305ff84bff1f2747816c3a3c393635446ca7b
Tobias Richter <tr4711@gmail.com>
2018-06-17 21:45:41 +0200

    Expose evaluation metrics

--- src/tailor/clustering/__init__.py
+++ src/tailor/clustering/__init__.py
@@ -1,7 +1,10 @@
-from .cluster import cluster # noqa
 from .cluster import build_clusters # noqa
+from .cluster import cluster # noqa
 from .cluster import cluster_characteristics # noqa
-from .ranking import rank_features # noqa
+from .cluster import output_clusters # noqa
+from .distance import derivative_euclidean # noqa
 from .distance import euclidean # noqa
 from .distance import interpolate_function # noqa
-from .distance import derivative_euclidean # noqa
+from .ranking import rank_features # noqa
+from .evaluation import cluster_scatter # noqa
+from .evaluation import cluster_separation # noqa
