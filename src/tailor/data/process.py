4486a71888dee27db036b689df06bbd2e8becba6
Tobias Richter <tr4711@gmail.com>
2018-07-08 23:37:00 +0200

    Align to single hyphens

--- src/tailor/data/process.py
+++ src/tailor/data/process.py
@@ -34,14 +34,14 @@ def transform_datatypes(df):
 
     print("Transforming datatypes")
     # Set article_id as category despite it's beeing a number
-    df["article_id"] = df["article_id"].astype('category')
+    df['article_id'] = df['article_id'].astype('category')
 
     # Set dates as type datetime
-    df["transaction_date"] = pd.to_datetime(df["transaction_date"])
-    df["markdown_start_date"] = pd.to_datetime(df["markdown_start_date"])
+    df['transaction_date'] = pd.to_datetime(df['transaction_date'])
+    df['markdown_start_date'] = pd.to_datetime(df['markdown_start_date'])
     # Replace 'infity' date with computable date in the future
-    df.loc[df.markdown_end_date == "9999-12-31", "markdown_end_date"] = "2018-12-31"
-    df["markdown_end_date"] = pd.to_datetime(df["markdown_end_date"])
+    df.loc[df.markdown_end_date == '9999-12-31', 'markdown_end_date'] = '2018-12-31'
+    df['markdown_end_date'] = pd.to_datetime(df['markdown_end_date'])
 
     # Set all other types as categories
     df[df.select_dtypes(['object']).columns] = df.select_dtypes(['object']).apply(lambda x: x.astype('category'))
