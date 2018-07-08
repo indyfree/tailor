ead8714f0e75c41a46f2b23f2f4bad9d40162d8d
Tobias Richter <tr4711@gmail.com>
2018-07-08 23:33:51 +0200

    Set categorical values right

--- src/tailor/data/process.py
+++ src/tailor/data/process.py
@@ -7,7 +7,13 @@ def fill_missing_values(df):
     Fill the missing values with 0 values.
     '''
 
-    print("Add missing time on sale values")
+    print("Add missing time_on_sale values")
+
+    # Save categorical values per article_id beforehand
+    groupers = df.select_dtypes(['category']).columns.tolist()
+    article_cats = df.groupby(by=groupers, as_index=False, observed=True).mean()
+    article_cats = article_cats.select_dtypes(['category'])
+
     df.set_index(['article_id', 'time_on_sale'], inplace=True)
 
     # Create MultiIndex with all article_ids and all 182 time on sale values
@@ -17,8 +23,10 @@ def fill_missing_values(df):
 
     # Reindex and fill values for previously indefined time_on_sale rows with 0
     df = df.reindex(idx, fill_value=0).reset_index()
+    df = df.select_dtypes(exclude=['category'])
     df["article_id"] = df["article_id"].astype('category')
-    return df
+
+    return df.merge(article_cats, on='article_id', how='inner')
 
 
 def transform_datatypes(df):
