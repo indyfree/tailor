3a3044ed9e9655af1ec7c985ad30ea722b21b9d4
Tobias Richter <tr4711@gmail.com>
2018-07-11 00:03:57 +0200

    Recalculate sells price and discount

--- src/tailor/data/group_by.py
+++ src/tailor/data/group_by.py
@@ -8,39 +8,38 @@ def weeks_on_sale(df):
     if 'weeks_on_sale' not in df.columns:
         raise ValueError("Cannot group on 'weeks_on_sale', not a column")
 
+    print("Group time_on_sale to weeks")
+
     # Select groupers (include categorical features to not drop them)
     groupers = df.select_dtypes(['category']).columns.tolist()
     groupers.append('weeks_on_sale')
 
     # Group dataframe and aggreagate with respective measures
     grouped = df.groupby(by=groupers, as_index=False, sort=False, observed=True)
-    df = grouped.agg({'original_price': np.max,
-                      'discount': lambda x: np.mean(x[x > 0]),
-                      'markdown': lambda x: np.mean(x[x > 0]),
+
+    df = grouped.agg({'original_price': np.mean,
+                      'markdown': np.mean,
                       'stock_total': np.max,
                       'article_count': np.sum,
                       'revenue': np.sum,
-                      # avq is the mean of all (non-added, zero) values
-                      'avq': lambda x: np.mean(x[x > 0])})
+                      'avq': np.mean})
 
-    # Recalculate the sales price according to formular to eliminate negative sales prices and establish data consistency
-    df['sells_price'] = df.apply(lambda row: (row['original_price'] - row['discount'] - row['markdown']), axis=1)
+    # Recalculate the sells price from revenue and article_count
+    df['sells_price'] = df.apply(lambda x: x.revenue / x.article_count, axis=1).round(decimals=2)
+
+    # Recalculate discount according to formular for consistency
+    df['discount'] = df.apply(lambda x: x.original_price - x.markdown - x.sells_price, axis=1)
+
+    # Round avq
+    df['avq'] = df.avq.round(decimals=2)
 
     # Weeks_on_sale is the new time_on_sale
     df = df.rename({'weeks_on_sale': 'time_on_sale'}, axis=1)
 
-    #
-    # Aesthetics
-    #
-
-    # Reorder columns
+    # Bring columns in correct order again
     columns = ['article_id', 'time_on_sale', 'original_price', 'discount', 'markdown', 'sells_price', 'stock_total', 'avq',
                'article_count', 'revenue', 'brand', 'color', 'Abteilung', 'WHG', 'WUG', 'month', 'season']
     df = df[columns]
-
-    # Round values
-    df['avq'] = df.avq.round(decimals=2)
-    df['revenue'] = df.revenue.round(decimals=0)
 
     return df
 
