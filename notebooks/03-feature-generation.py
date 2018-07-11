6dcc00137a582619d4a2671e1f201719c05458ca
Tobias Richter <tr4711@gmail.com>
2018-07-11 17:48:09 +0200

    Rerun notebooks

--- notebooks/03-feature-generation.py
+++ notebooks/03-feature-generation.py
@@ -60,19 +60,19 @@ get_ipython().run_cell_magic('time', '', '\n# expand transaction_date informatio
 get_ipython().run_cell_magic('time', '', '\nnew_seasons = df_group.apply(lambda x : get_season(x.transaction_date.min().month))')
 
 
-# In[21]:
+# In[9]:
 
 
 get_ipython().run_cell_magic('time', '', "\ndf['season'] = df['article_id'].apply(lambda x : new_seasons[x])")
 
 
-# In[17]:
+# In[10]:
 
 
 get_ipython().run_cell_magic('time', '', "\ndf = df.merge(pd.DataFrame(new_seasons), left_on='article_id', right_index=True)")
 
 
-# In[13]:
+# In[11]:
 
 
 df.head()
@@ -80,19 +80,19 @@ df.head()
 
 # ### timing on small scale
 
-# In[22]:
+# In[12]:
 
 
 df = df.head().copy()
 
 
-# In[23]:
+# In[13]:
 
 
 get_ipython().run_cell_magic('time', '', '\n# expand transaction_date information\nseason = list()\nweekday = list()\nmonths = list()\n\nfor i in df.transaction_date:\n    month = i.month\n    # meteorological seasons\n    if 2 < month < 6:\n        season.append(\'spring\')\n    elif 5 < month < 9:\n        season.append("summer")\n    elif 8 < month < 12:\n        season.append("fall")\n    else:\n        season.append("winter")\n\n    months.append(month)\n    day = i.weekday()\n    weekday.append(day)\n\ndf[\'season_buy\'] = pd.Series(season, index=df.index)\ndf[\'month\'] = pd.Series(months, index=df.index)\ndf[\'weekday\'] = pd.Series(weekday, index=df.index)')
 
 
-# In[24]:
+# In[14]:
 
 
 get_ipython().run_cell_magic('time', '', '\n# expand transaction_date information\n\n\ndef get_season(month):\n    if 2 < month < 6:\n        return \'spring\'\n    elif 5 < month < 9:\n        return "summer"\n    elif 8 < month < 12:\n        return "fall"\n    else:\n        return "winter"\n\n\ndf[\'month\'] = df[\'transaction_date\'].apply(lambda x: x.month)\ndf[\'season_buy\'] = df[\'month\'].apply(lambda x: get_season(x))\ndf[\'weekday\'] = df[\'transaction_date\'].apply(lambda x: x.weekday())')
