26129adc03196612c5254089700296e506c12e64
Tobias Richter <tr4711@gmail.com>
2018-07-16 00:18:35 +0200

    Fix variance calculation

--- src/tailor/clustering/__init__.py
+++ src/tailor/clustering/__init__.py
@@ -2,6 +2,7 @@ from .cluster import single_feature # noqa
 from .cluster import build_clusters # noqa
 from .cluster import cluster_characteristics # noqa
 from .cluster import multi_feature_split # noqa
+from .ranking import inter_feat_variance # noqa
 from .ranking import rank_features # noqa
 from .distance import absolute # noqa
 from .distance import interpolate_function # noqa
