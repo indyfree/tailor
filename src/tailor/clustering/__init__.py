d455c40285619ce2df4f1c72e2226123953589f0
Tobias Richter <tr4711@gmail.com>
2018-07-16 22:50:53 +0200

    Revert "Move multi-feature clustering into own file"
    This reverts commit c0c3c3d41aee263ae4ac54be07d31563a534095e.

--- src/tailor/clustering/__init__.py
+++ src/tailor/clustering/__init__.py
@@ -1,9 +1,9 @@
-from .cluster import cluster # noqa
+from .cluster import single_feature # noqa
 from .cluster import build_clusters # noqa
 from .cluster import merge_min_clusters # noqa
 from .cluster import merge_closest_cluster # noqa
 from .cluster import cluster_characteristics # noqa
-from .multi_cluster import multi_feature_split # noqa
+from .cluster import multi_feature_split # noqa
 from .ranking import inter_feat_variance # noqa
 from .ranking import rank_features # noqa
 from .distance import absolute # noqa
