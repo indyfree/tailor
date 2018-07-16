c0c3c3d41aee263ae4ac54be07d31563a534095e
Tobias Richter <tr4711@gmail.com>
2018-07-16 21:50:10 +0200

    Move multi-feature clustering into own file

--- src/tailor/clustering/__init__.py
+++ src/tailor/clustering/__init__.py
@@ -1,7 +1,7 @@
-from .cluster import single_feature # noqa
+from .cluster import cluster # noqa
 from .cluster import build_clusters # noqa
 from .cluster import cluster_characteristics # noqa
-from .cluster import multi_feature_split # noqa
+from .multi_cluster import multi_feature_split # noqa
 from .ranking import inter_feat_variance # noqa
 from .ranking import rank_features # noqa
 from .distance import absolute # noqa
