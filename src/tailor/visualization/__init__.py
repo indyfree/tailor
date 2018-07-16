20435d176cdf747972f42e36b8d727d6130e3ca5
Tobias Richter <tr4711@gmail.com>
2018-07-16 22:53:03 +0200

    Revert "Add PCA visualization method"
    This reverts commit b9eab9604930cec9026c255c8273ac90a0ef3839.

--- src/tailor/visualization/__init__.py
+++ src/tailor/visualization/__init__.py
@@ -2,4 +2,3 @@ from .visualize import plot_articles  # noqa
 from .visualize import plot_cluster_articles  # noqa
 from .visualize import plot_feature_characteristics  # noqa
 from .visualize import plot_cluster_characteristics  # noqa
-from .visualize import plot_cluster_pca  # noqa
