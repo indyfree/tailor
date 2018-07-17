4760f1d2d66df75ff37c9c119494d9e3c7257ba7
Tobias Richter <tr4711@gmail.com>
2018-07-17 10:34:56 +0200

    Set clusters on consequitive runs correctly

--- src/tailor/clustering/single_feature_cluster.py
+++ src/tailor/clustering/single_feature_cluster.py
@@ -7,70 +7,39 @@ def build_clusters(df, feature, distance_measure, distance_target):
     ''' Build clusters from the features characteristics '''
 
     df_cluster = data.group_by.feature(df, feature)
+
+    # Drop eventually existing cluster labels
+    if 'cluster' in df_cluster.columns:
+        df_cluster.drop(['cluster'], axis=1, inplace=True)
+
     # Assign the initial clusters, where each characteristic forms a cluster
     df_cluster['cluster'] = df_cluster[feature].cat.codes
+
     # Merge closest clusters till there are no close clusters anymore
     while True:
         distances = cluster_distances(df_cluster, distance_measure, distance_target)
+
         # Set the merging threshold as the values which fall below half of the average distance
         # NOTE: This is arbitrary and should be statistically proven
         threshold = distances.cluster_distance.mean() / 2
+
         a, b = closest_clusters(distances, threshold)
         if (a or b) is None:
             break
+
         # Merge the two closest clusters with a distance value below the threshold
         df_cluster.loc[df_cluster.cluster == a, 'cluster'] = b
+
     # Find out which feature characteristics are assigned to which cluster
-    char_to_cluster_map = df_cluster[[feature, 'cluster']].groupby(feature).mean()
+    char_to_cluster_map = df_cluster[[feature, 'cluster']].groupby(feature).first()
+
+    # Drop eventually existing cluster labels
+    if 'cluster' in df.columns:
+        df.drop(['cluster'], axis=1, inplace = True)
     # Add Cluster label to the original (article-level) dataframe
     df = df.merge(char_to_cluster_map, on=feature)
 
     return df
-
-
-def cluster_distances(df_cluster, distance_measure, distance_target):
-    '''
-    Takes a grouped dataframe with cluster labels and return a dataframe
-    with the distances between each pair of clusters.
-    '''
-
-    cluster = df_cluster.cluster.unique()
-    distances = []
-    # Loop over each cluster a and find out distance to every other cluster b
-    for i, a in enumerate(cluster):
-        a_curve = df_cluster.loc[df_cluster.cluster == a].set_index('time_on_sale')
-        for k, b in enumerate(cluster):
-            if k <= i:
-                continue
-            b_curve = df_cluster.loc[df_cluster.cluster == b].set_index('time_on_sale')
-            # Calculate distance between cluster a and b
-            d = distance_measure(a_curve[distance_target], b_curve[distance_target])
-            distances.append((a, b, d))
-
-    return pd.DataFrame(distances, columns=['from', 'to', 'cluster_distance'])
-
-
-def closest_clusters(distances, threshold):
-    ''' Return the two closest clusters '''
-
-    distances = distances.loc[distances.cluster_distance < threshold]
-    min_distance = distances.loc[distances.cluster_distance == distances.cluster_distance.min()]
-
-    if(not min_distance.empty):
-        return min_distance['to'].values[0], min_distance['from'].values[0]
-    else:
-        return None, None
-
-
-def cluster_characteristics(df, feat):
-    ''' Returns cluster information in a dataframe '''
-
-    c = pd.DataFrame()
-    c['num_articles'] = df.groupby(['cluster']).apply(lambda x: len(x['article_id'].unique()))
-    c['num_characteristics'] = df.groupby(['cluster']).apply(lambda x: len(x[feat].unique()))
-    c['characteristics'] = df.groupby(['cluster']).apply(lambda x: x[feat].unique().tolist())
-
-    return c
 
 
 def merge_min_clusters(df, feature, min_cluster_size, distance_measure, distance_target):
@@ -82,6 +51,7 @@ def merge_min_clusters(df, feature, min_cluster_size, distance_measure, distance
         c.sort_values(by=['num_articles'], ascending=True, inplace=True)
         min_cluster = c.index[0]
         df = merge_closest_cluster(df, feature, min_cluster, distance_measure, distance_target)
+
         c = pd.DataFrame()
         c['num_articles'] = df.groupby(['cluster']).apply(lambda x: len(x['article_id'].unique()))
 
@@ -89,13 +59,13 @@ def merge_min_clusters(df, feature, min_cluster_size, distance_measure, distance
 
 
 def merge_closest_cluster(df, feature, cluster, distance_measure, distance_target):
-
     df_cluster = data.group_by.feature(df, feature)
+
     clusters = df_cluster.cluster.unique()
     distance = sys.maxsize
     target_cluster = cluster
-    cluster_curve = df_cluster.loc[df_cluster.cluster == cluster].set_index('time_on_sale')
 
+    cluster_curve = df_cluster.loc[df_cluster.cluster == cluster].set_index('time_on_sale')
     # Loop over each cluster c and find out distance to the observed cluster
     for i, c in enumerate(clusters):
         # No need to compare to itself
@@ -104,7 +74,6 @@ def merge_closest_cluster(df, feature, cluster, distance_measure, distance_targe
 
         c_curve = df_cluster.loc[df_cluster.cluster == c].set_index('time_on_sale')
         d = distance_measure(cluster_curve[distance_target], c_curve[distance_target])
-
         if d < distance:
             distance = d
             target_cluster = int(c)
@@ -112,5 +81,3 @@ def merge_closest_cluster(df, feature, cluster, distance_measure, distance_targe
     if target_cluster is not cluster:
         # Merge the two clusters together
         df.loc[df.cluster == cluster, 'cluster'] = target_cluster
-
-    return df
