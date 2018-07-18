a96f1a2b499f351b464985071bd403b75c95a699
Christoph Alexander Bullmann <bullmanc@smail.uni-koeln.de>
2018-07-18 18:32:29 +0200

    rewrite cluster algorithm to be sound and sturdy

--- notebooks/10-presentation-notebook.py
+++ notebooks/10-presentation-notebook.py
@@ -205,6 +205,8 @@ plot_articles(df, [900001, 900002, 900030], 'avq');
 
 # In comparison to the raw data, you can see that the graphs no longer looks that messy. It is easier to identify which graphs are similar. Moreover we see that we removed seasonal effects by grouping the daily sales to weeks.
 
+# # The Clustering Algorithm
+
 # ## Inter-Feature Variance
 
 # Remember that we want to identify a (sub-)population of similar articles that we can use for prediction or further analysis. 
@@ -255,46 +257,6 @@ inter_feat_variance(df, distance.absolute, 'Abteilung', 'norm_article_count')
 inter_feat_variance(df, distance.absolute, 'color', 'norm_article_count')
 
 
-# # The Clustering Algorithm
-
-# ## General
-
-# The general idea of the clustering algorithm is to recursively split the article population into the characteristics of each feature. Afterwards "similar" characteristics are merged together to clusters. This is a way of hierarchical clustering: With each considered feature the number of clusters increases and the size of the clusters grows smaller.
-# 
-# 
-# In the end we will have clusters that look similar to this:
-# 
-# * Level 1
-#     * __Cluster 1__:  
-#         * _Brand_: Adidas
-#     * __Cluster 2__:  
-#         * _Brand_: Nike, Rebook 
-#         
-# * Level 2
-#     * __Cluster 1.1__:  
-#         * _Brand_: Adidas
-#         * _Color_: blue, red 
-#     * __Cluster 1.2__:  
-#         * _Brand_: Adidas
-#         * _Color_: green
-#     * __Cluster 2.1__:
-#         * _Brand_: Nike, Rebook
-#         * _Color_: red
-#     * __Cluster 2.2__:  
-#         * _Brand_: Nike, Rebook
-#         * _Color_: blue, green
-#         
-# * Level 3:
-#     * __Cluster 1.1.1__:  
-#         * _Brand_: Adidas
-#         * _Color_: blue, red
-#         * _Season_: autumn 
-#     * ...
-#     
-# Note: The features don't have to be split the same way across all cluster. See e.g. _Cluster 1.1_ vs. _Cluster 2.1_.  The feature color has been split differently. E.g. "Red and blue Adidas shoes are similar, but red Nikes and Rebooks are not similar to blue Nikes and Rebooks"
-# 
-# The challenge is a) to rank the features in the best order we want to consider them (e.g. first _Brand_ then _Color_) and b) find similar behaving characteristics (e.g. blue and red Adidas are similar).
-
 # ## Similarity Measure
 
 # Each clustering algorithm needs to define a similarity measure, also called "distance", to cluster similar items together. In our case "similar" articles are articles, that showed similar selling behavior while being on sale in the shops of the customer. The dataset yields measures for the number of articles sold over time (*article count*) the generated *revenue* and the sales quota in comparison to the stock (_avq_, _Abverkaufsquote_). So in the use-case of our customer similar articles are the ones that have a similar curve of *revenue*, *article_count* or *avq* over time.
@@ -339,66 +301,53 @@ b = df.loc[df.article_id == 900050].set_index(
 print("distance: ", distance.absolute(a, b))
 
 
-# ## Outline of the Algorithm
+# ## The First Idea
 
-# Remember we are searching article groups, characterized by their attributes (color, brand, etc..) that are similar to each other. We previously defined similarity, showed the structure of the resulting clusters, and discussed the categorical as well as the time-series part of the clustering. Now we will illustrate the top-level steps the clustering algorithm actually does.
+# The general idea of the clustering algorithm is to split the article population into the characteristics of each feature. Afterwards "similar" characteristics are merged together to clusters. These clusters can then be split by a different feature. This is a way of hierarchical clustering: With each considered feature the number of clusters increases and the size of the clusters diminishes.
 # 
 # 
-# ```python
-# # 1. Get a list of ranked features (color, brand, etc..) 
-# #    according to their inter_feature score
-# features =  rank_features()
+# In the end we will have clusters that look similar to this:
 # 
-# # Repeat until all features considered
-# for f in features:
-#     # 2. Split feature characteristics into separate clusters
-#     split_features()
-#    
-#     # 3. Merge similar characteristics together into a cluster. Merge when the distance
-#     #    between two cluster is less then a 'similarity_threshold' 
-#     merge_close_clusters()
+# * Level 1
+#     * __Cluster 1__:  
+#         * _Brand_: Adidas
+#     * __Cluster 2__:  
+#         * _Brand_: Nike, Rebook 
+#         
+# * Level 2
+#     * __Cluster 1.1__:  
+#         * _Brand_: Adidas
+#         * _Color_: blue, red 
+#     * __Cluster 1.2__:  
+#         * _Brand_: Adidas
+#         * _Color_: green
+#     * __Cluster 2.1__:
+#         * _Brand_: Nike, Rebook
+#         * _Color_: red
+#     * __Cluster 2.2__:  
+#         * _Brand_: Nike, Rebook
+#         * _Color_: blue, green
+#         
+# * Level 3:
+#     * __Cluster 1.1.1__:  
+#         * _Brand_: Adidas
+#         * _Color_: blue, red
+#         * _Season_: autumn 
+#     * ...
 #     
-#     # 4. Merge clusters that are below the min_cluster_size into the closest cluster
-#     merge_min_clusters()
-#     
-#     # 5. When no new clusters resulted from the considered feature finish the clustering
-#     #    (e.g. all new clusters were too small)
-#     if no_new_clusters:
-#         break
-# ```
+# Note: The features don't have to be split the same way across all cluster. See e.g. _Cluster 1.1_ vs. _Cluster 2.1_.  The feature color has been split differently. E.g. "Red and blue Adidas shoes are similar, but red Nikes and Rebooks are not similar to blue Nikes and Rebooks"
 # 
-# In words, we are considering every feature, ranked by its variance score. The features characteristics form a cluster, which then get merged with similar characteristics (Remember we want the clusters to be as similar as possible). The merging is going to happen more frequently towards the end of the `ranked_feature list`, since the feature characteristics are closer to another (low variance). Furthermore we are looking at the size of the clusters, and merge clusters which are too small (Remember we want the clusters to be as big as possible). The `min_cluster_size` and the `similarity_threshold` are parameters and can be set according to the customers preferences. Of course there is always a trade-of between similarity and size of a cluster.
+# The challenge is a) to rank the features in the best order we want to consider them (e.g. first _Brand_ then _Color_) and b) find similar behaving characteristics (e.g. blue and red Adidas are similar).
 
-# ## Hierarchical Component
+# ### Outline of the Algorithm
 
-# (Here some kind of visualization of the clustering process would be cool)
-
-# # Results and Evaluation
-
-# To discuss the results we will pick out an potential use-case of a customer and go through the whole clustering process. 
-# 
-# Assuming we are a fashion retailer and want to introduce a new special edition shoe **Breeezy 5000** into our shops. Beforehand, we want to know how much **revenue** this article is likely going to generate and how the **revenue curve** will likley look like over the course of the 26 weeks this special shoe is available in our shops. In order to let our data science team do all the prediction work, we have to give them a reference population of shoes that are similar to our special edition shoe. Luckily we have paid the smart guys from tailorit to find this reference population, by using a **cluster analysis**. The smart guys from tailorit have developed a clustering package called **tailor** which they going to use for the analysis.
-
-# ## Define The Use Case
-
-# Our new shoes have a few attributes, like brand and color, are of a certain type (WUG, WHG) and will be placed in a certain compartment (Abteilung) of our shops. Moreover we want to introduce it in August.
-# 
-# Consequently we assume the following attributes for our **Breeezy 5000**:
-# - brand: **Hödur**
-# - color: **mittelbraun**
-# - Abteilung: **Abteilung005**
-# - WHG: **WHG014**
-# - WUG: **WUG073**
-# - month: **August**
-# - season: **Sommer**
-
-# ### Set Clustering Parameters
+# #### Set Clustering Parameters
 
 # In[20]:
 
 
-# Use standardized revenue as measure for clustering
-target_value = 'norm_revenue'
+# Use standardized article count as measure for clustering
+target_value = 'norm_article_count'
 min_cluster_size = 50
 # We want to observe all features available
 features = ['color', 'brand', 'Abteilung', 'WHG', 'WUG', 'season', 'month']
@@ -406,7 +355,7 @@ features = ['color', 'brand', 'Abteilung', 'WHG', 'WUG', 'season', 'month']
 distance_measure = distance.absolute
 
 
-# ### Rank the Features
+# #### Rank the Features
 
 # In[21]:
 
@@ -417,38 +366,44 @@ feats
 
 # We can see here that 'WUG' is the most informative feature for us. The **inter-feature variance** is the highest, which means that the single 'WUG's are far apart from each other, thus making good cluster candidates
 
-# ## Building First Clusters (Level 1)
+# #### Building First Clusters (Level 1)
 
-# We start by building cluster using the most informative feature.
-
-# In[22]:
-
-
-# Select important feature
-feat = feats.iloc[1].feature
-
-# Build Cluster at Level 1
-c1 = build_clusters(df, feat, distance_measure, target_value)
-cluster_characteristics(c1, feat)
-
-
-# After first clustering step, we can see that some big clusters (0, 1, 8, 12) and several small clusters have been formed. Since we want to generate clusters, that fulfill a minimum sample size, we now need to merge clusters that fall under the `min_cluster_size` with the respective closest cluster.
+# We start building clusters using the most informative feature.
 
 # In[23]:
 
 
-c1 = merge_min_clusters(c1, feat, min_cluster_size,
-                        distance.absolute, target_value)
+get_ipython().run_cell_magic('time', '', '\n# Select important feature\nfeat = feats.iloc[0].feature\n\n# Build Cluster at Level 1\nc1 = build_clusters(df, feat, distance_measure, target_value)')
+
+
+# In[24]:
+
+
 cluster_characteristics(c1, feat)
 
 
-# We can see now that only clusters above the `min_cluster_size` remain. All other clusters have been merged with neighbouring clusters.
+# After first clustering step, we can see that some big clusters (0, 1, 3) and several small clusters have been formed. Since we want to generate clusters, that fulfill a minimum sample size, we now need to merge clusters that fall under the `min_cluster_size` with the respective closest cluster.
+# We also see that the first clustering step has taken 18 minutes, which should be improvable.
+
+# In[26]:
+
+
+get_ipython().run_cell_magic('time', '', '\nc1 = merge_min_clusters(c1, feat, min_cluster_size,\n                        distance.absolute, target_value)')
+
+
+# In[27]:
+
+
+cluster_characteristics(c1, feat)
+
+
+# We can see now that only clusters above the `min_cluster_size` remain. All other clusters have been merged with neighboring clusters. Additionally, the time needed for the merging is negligible compared to the initial clustering step. 
 
 # ### Plotting the Clusters
 
 # Visualizing the results we can see that the mean curves of the clusters are quite different. They follow a similar trend but have different course over time.
 
-# In[24]:
+# In[28]:
 
 
 plot_feature_characteristics(c1, 'cluster', target_value);
@@ -458,28 +413,40 @@ plot_feature_characteristics(c1, 'cluster', target_value);
 
 # Principal Component Analysis (PCA) is a common technique for feature reduction and visualization of multi-dimensional data in 2D. PCA transform the data onto new "orthogonal" axis, along the axis where there is the largest variance in the original data (the revenue curve with the 26 dimensions, one for each day). Time-series data is not the main use-case for PCA, but nevertheless we can see more than 50% of the variance explained by the first two components. 
 # 
-# Using PCA for visualization, and plotting the individual articles for the clusters we can see that the current split alone is not sufficient for a distinct difference between all clusters. However, when we select certain clusters a distinction can be made.
+# Using PCA for visualization, and plotting the individual articles for the clusters we can see that PCA does not seem to be suitable for our data. However, when we select certain clusters a distinction can be made, which should provide some validation to our methods.
 
-# In[25]:
-
-
-plot_cluster_pca(c1, [0, 1, 3, 5, 6, 7, 12, 16, 17, 24, 37, 38], target_value);
+# In[31]:
 
 
-# In[59]:
+plot_cluster_pca(c1, [0, 1, 3, 5, 7, 10, 14, 27, 64], target_value);
 
 
-plot_cluster_pca(c1, [7, 17, 16, 37], target_value);
+# In[42]:
 
 
-# ## Further Clustering (Level 2)
+plot_cluster_pca(c1, [5, 14, 64], target_value);
 
-# The first clustering step produced clusters that are characterized by *WHG*. We can use reference population for our new shoe, by finding the cluster, which groups articles that have the same *WHG* as the **Breeezy 5000**.
-# In this case its *Cluster 0*, which includes WHG014.
+
+# #### Applied to a Use Case
+
+# To make the clustering process more tangible we assume that we are a fashion retailer and want to introduce a new special edition shoe **Breeezy 5000** into our shops. Beforehand, we want to know how many shoes need to be produced how quickly. To satisfy this need we use the **norm_article_count** clustering to find a suitable target set for initial inventory prediction. By looking at the **article count curve** of a shoe within the cluster a decent starting point should be made.
+# 
+# 
+# We assume the following attributes for our **Breeezy 5000**:
+# - brand: **Skuld**
+# - color: **mittelbraun**
+# - Abteilung: **Abteilung005**
+# - WHG: **WHG014**
+# - WUG: **WUG073**
+# - month: **August**
+# - season: **Sommer**
+
+# The first clustering step produced clusters that are characterized by *WUG*. We can use reference population for our new shoe, by finding the cluster, which groups articles that have the same *WHG* as the **Breeezy 5000**.
+# In this case its *Cluster 0*, which includes WUG073.
 # 
 # We further want to inspect the cluster size and variance of *Cluster 0*:
 
-# In[26]:
+# In[43]:
 
 
 print("Number of articles: %s" %
@@ -487,20 +454,20 @@ print("Number of articles: %s" %
 print("Variance: %s" % cluster_variance(c1, 0, distance_measure, target_value))
 
 
-# Our shoe is in a relatively large cluster, where the variance is close to 1. We believe that we can find a better reference population by clustering further and looking at more features.
+# Our shoe is in a relatively large cluster, where the variance is larger than 1. We believe that we can find a better reference population by clustering further:
 
-# ### Redefine the Article Population
+# #### Redefine the Article Population
 
-# In[27]:
+# In[44]:
 
 
 # We looking to Cluster 0
 c2 = c1.loc[c1.cluster == 0]
 
 
-# ### Cluster with the Next Feature
+# #### Cluster with the Next Feature
 
-# In[28]:
+# In[45]:
 
 
 # For simplicity we define a function for a clustering step
@@ -514,32 +481,53 @@ def cluster_step(c):
     return (c, feat)
 
 
-# In[29]:
+# In[50]:
 
 
-c2, feat = cluster_step(c2)
+get_ipython().run_cell_magic('time', '', '\nc2, feat = cluster_step(c2)')
+
+
+# In[51]:
+
+
 cluster_characteristics(c2, feat)
 
 
-# In[30]:
+# In[54]:
 
 
 print("Number of Articles: %s" %
-      cluster_characteristics(c2, feat).loc[34].num_articles)
+      cluster_characteristics(c2, feat).loc[51].num_articles)
 print("Variance: %s" % cluster_variance(
-    c2, 34, distance_measure, target_value))
+    c2, 51, distance_measure, target_value))
 
 
-# In[31]:
+# In[55]:
 
 
 plot_feature_characteristics(c2, 'cluster', target_value);
 
 
-# The second level clustering produced a cluster that has a lot less variance (0.66 in comparison to 0.99), while still having a decent cluster size (87). Looking at the visual output we can see that the **Cluster 34** has a distinct curve than the other clusters.
+# The second level clustering for cluster 0 produced a cluster (51) that has a lot less variance (0.611 in comparison to 1.13), while still having a decent cluster size (81). Looking at the visual output we can see that the **Cluster 51** has a distinct curve when compared to the other clusters.
 
-# ### Evaluation
+# While the normalized article_count is used by the algorithm, the raw article_count would be used to determine the real inventory size:
 
-# Through clustering on two hierarchical levels we found an article population that is above the set `min_cluster_size` and has a low variance. The articles in that cluster are similar in their selling behavior and make a good basis for further predictions or analysis. Assuming a market introduction of the **Breezy 5000** we can use this population to determine strategies for a wanted 
+# In[57]:
+
+
+plot_feature_characteristics(c2, 'cluster', 'article_count');
+
+
+# ## The Second Approach
+
+# Since the first algorithm is taking too much time for the single-feature-clustering and one has to manually iterate through the levels of clustering, a second try has been made. 
+# 
+# While the basic idea to split the data based on *inter-feature-variance* and merge *similar* clusters together is still driving the clustering, the second approach splits the data for all available features first - before any merging has taken place. 
+# 
+# The merging itself also changed to a more efficient version, while approach number one only merges two clusters before recalculating distances, approach number two merges every cluster to its closest neighbor and recalculates distances afterwards. 
+# 
+# These changes in combination with a new data structure result in superior performance. Additionally, not only *min_cluster_size* is taken into account, but also *max_cluster_count*.
+# 
+# Besides the efficiency improvement, small subgroups (e.g. from split level 4 and 6) of e.g. split level 1 cluster 0 and cluster 2 can be merged, taking a step away from pure hierarchical clustering, which could be seen as a less restrictive approach and therefore considered beneficial for cluster quality. However, this also dilutes the feature specificity of the clusters, the first approach results in clusters that are 100% identified by a certain feature characteristic combination.
 
 # # Outlook and Discussion
